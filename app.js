a693813ca7c3328eb35919f9f6cb9e4ed27af220
grey1man <49520795+grey1man@users.noreply.github.com>
2019-05-12 00:41:01 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const math = require("C:/users/bss/AppData/Roaming/npm/node_modules/mathjs");
+const math = require("C:/users/grey man/AppData/Roaming/npm/node_modules/mathjs");
 const app = express();  
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-var str = "z + 1= 7"
+var str = "6 - 5 *  z = 2"
 str = str.split('=') 
 var node1 = math.parse(str[0])
 var node2 = math.parse(str[1])
@@ -35,7 +35,7 @@ function inverse(op){
 }
 
 console.log('aaaa')
-console.log(math.parse('-x'))
+console.log(node1)
 console.log('aaaa')
 
 
@@ -44,27 +44,31 @@ node1.traverse(function (node, path, parent) {
     case 'OperatorNode':
       console.log(node.type, node.op, path, node.args[0].fn)
       console.log(node.clone())
-      if (node.fn == 'unaryMinus'){
+      if (node.fn == 'unaryMinus' && node.args[0].name == 'z'){
         node2 = new math.expression.node.OperatorNode('-', inverse(node.fn), [node2])
         return
       }
-      if (node.fn == 'unaryMinus'){
-        node2 = new math.expression.node.OperatorNode('-', inverse(node.fn), [node2])
+      if (node.args[0].type == 'OperatorNode' && node.args[0].fn == 'unaryMinus' ){
+        node2 = new math.expression.node.OperatorNode(inverse(node.op), inverse(node.fn), [node2, node.args[0]])
         return
       }
-      if ((node.args[0].type == 'ConstantNode' || node.args[0].type == 'SymbolNode') && node.args[0].name != 'z' && counter == 0){
+      if (node.fn != 'unaryMinus' && (node.args[0].type == 'ConstantNode' || node.args[0].type == 'SymbolNode') && node.args[0].name != 'z' && counter == 0 ){
         node2 = new math.expression.node.OperatorNode(inverse(node.op), inverse(node.fn), [node2, node.args[0]])
+        return
       }
-      if ((node.args[1].type == 'ConstantNode' || node.args[1].type == 'SymbolNode') && node.args[1].name != 'z' && counter == 0 ){ 
+      if (node.fn != 'unaryMinus' && (node.args[1].type == 'ConstantNode' || node.args[1].type == 'SymbolNode') && node.args[1].name != 'z' && counter == 0 ){ 
         node2 = new math.expression.node.OperatorNode(inverse(node.op), inverse(node.fn), [node2, node.args[1]])
+        return
       }
-      if (node.args[1].type == 'OperatorNode' && node.args[0].type == 'SymbolNode' && node.args[1].name != 'z'){
+      if (node.fn != 'unaryMinus' && node.args[1].type == 'OperatorNode' && node.args[0].type == 'SymbolNode' && node.args[1].name != 'z'){
         node2 = new math.expression.node.OperatorNode(inverse(node.op), inverse(node.fn), [node2, node.args[1]])
+        return
       }
-      if (node.args[0].type == 'OperatorNode' && node.args[1].type == 'SymbolNode' && node.args[1].name != 'z'){
+      if (node.fn != 'unaryMinus' && (node.args[0].type == 'OperatorNode' && node.args[1].type == 'SymbolNode' && node.args[1].name != 'z')){
         node2 = new math.expression.node.OperatorNode(inverse(node.op), inverse(node.fn), [node2, node.args[0]])
+        return
       }
-     
+      
      
       console.log(' ')
       console.log(' ')
